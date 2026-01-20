644aa6b48998b454db28f511a21845ccfc0dd4f0
Abdul Razzak <185785995+AbdulRazzak345@users.noreply.github.com>
2026-01-20 22:16:58 +0500

    Add files via upload

--- MainClass.java
+++ MainClass.java
@@ -1,27 +1,18 @@
+import java.util.Arrays;
+
 public class MainClass {
     public static void main(String[] args) {
+        int[] arr1 = {5, 1, 4, 2, 8};
+        int[] arr2 = Arrays.copyOf(arr1, arr1.length);  // same array for Quick Sort
 
-        int[] arr = {3, 8, 12, 20, 25, 30};
+        // Merge Sort
+        MergeSort ms = new MergeSort();
+        ms.mergeSort(arr1, 0, arr1.length - 1);
+        System.out.println("Sorted array (Merge Sort): " + Arrays.toString(arr1));
 
-        // Create object of SearchOperations
-        binarySearchIterative.SearchOperations searchOps = new binarySearchIterative.SearchOperations();
-
-        // 1. Iterative Binary Search for 20
-        int idx1 = searchOps.binarySearchIterative(arr, 20);
-        System.out.println("Iterative Binary Search: 20 found at index: " + idx1);
-
-        // 2. Recursive Binary Search for 25
-        int idx2 = searchOps.binarySearchRecursive(arr, 25, 0, arr.length - 1);
-        System.out.println("Recursive Binary Search: 25 found at index: " + idx2);
-
-        // 3. Recursive Linear Search for 12
-        int idx3 = searchOps.recursiveLinearSearch(arr, 12, 0);
-        System.out.println("Recursive Linear Search: 12 found at index: " + idx3);
-
-        // 4. Recursive Min and Max
-        int min = searchOps.findMin(arr, 0);
-        int max = searchOps.findMax(arr, 0);
-        System.out.println("Minimum: " + min);
-        System.out.println("Maximum: " + max);
+        // Quick Sort
+        QuickSort qs = new QuickSort();
+        qs.quickSort(arr2, 0, arr2.length - 1);
+        System.out.println("Sorted array (Quick Sort): " + Arrays.toString(arr2));
     }
 }
